ac8a716cb4936deae3ed8d758ed231e5d68b9981
tudordan25 <tudordan25@gmail.com>
2022-02-28 23:41:20 +0200

    Add ticket and search option

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -1,11 +1,87 @@
 package com.example.notesapp
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.SearchView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.ticket.view.*
 
 class MainActivity : AppCompatActivity() {
+    var listNotes=ArrayList<Note>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        listNotes.add(Note(1,"meet professor","This is the content text and it should be larger in order to see it's way of displaying on multiple rows"))
+        listNotes.add(Note(2,"meet doctor","This is the content text and it should be larger in order to see it's way of displaying on multiple rows"))
+        listNotes.add(Note(3,"meet friend","This is the content text and it should be larger in order to see it's way of displaying on multiple rows"))
+
+        var myNotesAdapter = MyNotesAdapter(listNotes)
+        lvNotes.adapter = myNotesAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val sv=menu!!.findItem(R.id.app_bar_search).actionView as SearchView
+        val sm=getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        sv.setSearchableInfo(sm.getSearchableInfo(componentName))
+        sv.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Toast.makeText(applicationContext, query, Toast.LENGTH_SHORT).show()
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+
+        })
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId) {
+            R.id.addNote->{
+                var intent = Intent(this,AddNotes::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    inner class MyNotesAdapter : BaseAdapter {
+        var listNotesAdapter=ArrayList<Note>()
+        constructor(listNotesAdapter:ArrayList<Note>):super(){
+            this.listNotesAdapter = listNotesAdapter
+        }
+
+        override fun getCount(): Int {
+            return listNotes.size
+        }
+
+        override fun getItem(position: Int): Any {
+            return listNotes[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            var myView = layoutInflater.inflate(R.layout.ticket, null)
+            var myNote = listNotes[position]
+            myView.tvTitle.text = myNote.noteName
+            myView.tvDes.text = myNote.noteDes
+            return myView
+        }
+
     }
 }
